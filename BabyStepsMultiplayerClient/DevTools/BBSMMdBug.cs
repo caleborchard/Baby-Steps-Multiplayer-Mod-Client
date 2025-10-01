a2822828d10e70b29422bd808e5624441d45e75b
caleborchard <caleborchard08@gmail.com>
2025-10-01 17:20:34 -0400

    Save DevTools/BBSMMdBug.cs before merge

---
+++ BabyStepsMultiplayerClient/DevTools/BBSMMdBug.cs
@@ -0,0 +1,24 @@
+﻿using MelonLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BabyStepsMultiplayerClient.Debug
+{
+    public class BBSMMdBug
+    {
+        public static bool debugEnabled = false;
+        private static string modLogPath = Path.Combine(MelonLoader.Utils.MelonEnvironment.UserDataDirectory, "bbsmm.log");
+        public static void ClearFile() { File.Delete(modLogPath); File.Create(modLogPath); }
+        public static void Log(string message) 
+        { 
+            if (debugEnabled)
+            {
+                MelonLogger.Msg("DEBUG:" + message);
+                File.AppendAllLines(modLogPath, new string[] { message });
+            }
+        }
+    }
+}
