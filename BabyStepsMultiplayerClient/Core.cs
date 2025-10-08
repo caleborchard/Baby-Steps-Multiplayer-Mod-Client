9b482720ff5a0db5aa64e5095a5bdbbab48ea1df
caleborchard <caleborchard08@gmail.com>
2025-10-08 09:09:20 -0400

    Bump version number

--- BabyStepsMultiplayerClient/Core.cs
+++ BabyStepsMultiplayerClient/Core.cs
@@ -9,7 +9,7 @@ using UnityEngine;
 
 [assembly: MelonInfo(typeof(BabyStepsMultiplayerClient.Core),
     "BabyStepsMultiplayerClient",
-    "1.1.2",
+    "1.1.3",
     "Caleb Orchard",
     "https://github.com/caleborchard/Baby-Steps-Multiplayer-Mod-Client")]
 [assembly: MelonGame("DefaultCompany", "BabySteps")]
